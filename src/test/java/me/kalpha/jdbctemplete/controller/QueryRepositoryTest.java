bb7035bd82a04617c7439a7e1057236ec3f5bf24
kalphageek <kalphageek@outlook.com>
2021-01-12 00:29:01 +0900

    LocalDate -> String/TO_DATE

--- src/test/java/me/kalpha/jdbctemplete/controller/QueryRepositoryTest.java
+++ src/test/java/me/kalpha/jdbctemplete/controller/QueryRepositoryTest.java
@@ -1,5 +1,6 @@
 package me.kalpha.jdbctemplete.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.kalpha.jdbctemplete.domain.QueryDto;
 import me.kalpha.jdbctemplete.domain.Row;
@@ -39,24 +40,29 @@ class QueryRepositoryTest {
     public void findByQuery_Api() throws Exception {
         String query = "select job_execution_id, version, job_instance_id, create_time, start_time, end_time\n" +
                 "from batch_job_execution\n" +
-                "where create_time >= ?\n" +
-                "\tand create_time < ?\n" +
+                "where 1 = 1\n" +
+                "\tand create_time >= to_date(?,'yyyy-MM-dd')\n" +
+                "\tand create_time < to_date(?,'yyyy-MM-dd') + 1\n" +
                 "\tand job_instance_id > ?\n" +
-                "\tand status in (?)\n" +
                 "\tand exit_code like ?\n" +
                 "\tand exit_message is not null and exit_message <> ''\n" +
+                "\tand status in (%s)\n" +
                 "order by job_instance_id desc, version desc";
 
-        LocalDate createTime1 = LocalDate.parse("2020-10-01");
-        LocalDate createTime2 = LocalDate.parse("2020-10-04").plusDays(1);
-        Object[] params = {createTime1, createTime2, 20, "FAILED", "%"+"FAIL"+"%"};
+        //in절 -->
+        List<String> inClouse = new ArrayList<>();
+        inClouse.add("FAILED");
+        inClouse.add("WARNNING");
+        StringBuilder queryBuilder = new StringBuilder();
+        for( int i = 0; i< inClouse.size(); i++){
+            queryBuilder.append("?");
+            if (i !=  inClouse.size() -1) queryBuilder.append(", ");
+        }
+        query = String.format(query, queryBuilder.toString());
+        //in절 <--
 
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-//        sdf.setTimeZone(TimeZone.getDefault());
-//        Object[] params = {sdf.parse("20201001")
-//                ,sdf.parse("20201030")
-//                , 20
-//                , "FAILED"};
+        System.out.println(query);
+        Object[] params = {"2020-10-01", "2020-10-04", 20, "%"+"FAIL"+"%", inClouse.get(0), inClouse.get(1)};
 
         QueryDto queryDto = new QueryDto();
         queryDto.setQuery(query);
@@ -74,16 +80,14 @@ class QueryRepositoryTest {
     public void findByQuery() throws Exception {
         String query = "select job_execution_id, version, job_instance_id, create_time, start_time, end_time\n" +
                 "from batch_job_execution\n" +
-                "where create_time >= ?\n" +
-                "\tand create_time < ?\n" +
+                "where 1 = 1\n" +
+                "\tand create_time >= to_date(?,'yyyy-MM-dd')\n" +
+                "\tand create_time < to_date(?,'yyyy-MM-dd') + 1\n" +
                 "\tand job_instance_id > ?\n" +
                 "\tand exit_code like ?\n" +
                 "\tand exit_message is not null and exit_message <> ''\n" +
                 "\tand status in (%s)\n" +
                 "order by job_instance_id desc, version desc";
-
-        LocalDate createTime1 = LocalDate.parse("2020-10-01");
-        LocalDate createTime2 = LocalDate.parse("2020-10-04").plusDays(1);
 
         //in절 -->
         List<String> inClouse = new ArrayList<>();
@@ -91,19 +95,19 @@ class QueryRepositoryTest {
         inClouse.add("WARNNING");
         StringBuilder queryBuilder = new StringBuilder();
         for( int i = 0; i< inClouse.size(); i++){
-            queryBuilder.append(" ?");
-            if (i !=  inClouse.size() -1) queryBuilder.append(",");
+            queryBuilder.append("?");
+            if (i !=  inClouse.size() -1) queryBuilder.append(", ");
         }
         query = String.format(query, queryBuilder.toString());
         //in절 <--
 
         System.out.println(query);
-
-        Object[] params = {createTime1, createTime2, 20, "%"+"FAIL"+"%", inClouse.get(0), inClouse.get(1)};
+        Object[] params = {"2020-10-01", "2020-10-04", 20, "%"+"FAIL"+"%", inClouse.get(0), inClouse.get(1)};
 
         QueryDto queryDto = new QueryDto();
         queryDto.setQuery(query);
         queryDto.setParams(params);
+
         List<Row> rowList = queryService.query(queryDto);
         rowList.stream().forEach(System.out::println);
 
