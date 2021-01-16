6da59465bb8e19199717c84ac47e0e83be66a9ee
kalphageek <kalphageek@outlook.com>
2021-01-16 22:42:32 +0900

    ..

--- src/main/java/me/kalpha/jdbctemplate/query/QueryRepositoryImpl.java
+++ src/main/java/me/kalpha/jdbctemplate/query/QueryRepositoryImpl.java
@@ -26,11 +26,27 @@ public class QueryRepositoryImpl implements QueryRepository {
     }
 
     /**
-     * 1개 Row만 Return 받을때는 rowList.stream().findFirst()또는 findAny() 사용.
-     * 두 메소드는 해당 스트림에서 첫 번째 요소를 참조하는 Optional 객체를 반환
+     * query를 validate한다
      * @param query
      * @param params
      * @return
+     */
+    @Override
+    public Boolean validateQueryByParams(String query, Object[] params) {
+        String valdateQuery = "SELECT ( EXISTS (SELECT 1)\n" +
+                "OR\n" +
+                "EXISTS (%s)\n" +
+                ") AS result";
+        valdateQuery = String.format(valdateQuery, query);
+        return jdbcTemplate.queryForObject(valdateQuery, params, Boolean.class);
+    }
+
+    /**
+     * 1개 Row만 Return 받을때는 rowList.stream().findFirst()또는 findAny() 사용.
+     * 두 메소드는 해당 스트림에서 첫 번째 요소를 참조하는 Optional 객체를 반환
+     * @param query 사용자 Query
+     * @param params Query 파라미터들
+     * @return 컬럼을 ArrayList로 가지는 ArrayList ResultSet
      */
     @Override
     public List findByParams(String query, Object[] params) {
@@ -38,6 +54,12 @@ public class QueryRepositoryImpl implements QueryRepository {
         return list;
     }
 
+    /**
+     * @param query 사용자 Query
+     * @param params Query 파라미터들
+     * @return 컬럼을 ArrayList로 가지는 ArrayList ResultSet를 Page객체에 담아서 리턴한다
+     * Pageable을 RequestParam로 받는 경우 API호출 패턴 : GET /items?after_id=20&limit=20&offset=1&sort_by=desc(last_modified),asc(email)
+     */
     @Override
     public Page<List> findByParams(Pageable pageable, String query, Object[] params) {
         String pagingQuery = String.format("select * from (%s) as t limit %d offset %d"
@@ -61,16 +83,11 @@ public class QueryRepositoryImpl implements QueryRepository {
         return jdbcTemplate.queryForObject(countQuery, params, Integer.class);
     }
 
-    @Override
-    public Boolean validateQueryByParams(String query, Object[] params) {
-        String valdateQuery = "SELECT ( EXISTS (SELECT 1)\n" +
-                "OR\n" +
-                "EXISTS (%s)\n" +
-                ") AS result";
-        valdateQuery = String.format(valdateQuery, query);
-        return jdbcTemplate.queryForObject(valdateQuery, params, Boolean.class);
-    }
-
+    /**
+     * 기준컬럼이 있는 테이블의 경우 기준컬럼으로 order by [기준컬럼] desc 해서 샘플을 가져온다
+     * @param tableName
+     * @return
+     */
     @Override
     public List findRecently(String tableName) {
         String query = String.format("select * from %s order by %s desc limit %d",tableName, HUB_BASE_COLUMN, DEFAULT_LIMITS);
@@ -78,6 +95,11 @@ public class QueryRepositoryImpl implements QueryRepository {
         return list;
     }
 
+    /**
+     * 기준컬럼이 있는 테이블의 경우 기준컬럼으로 order by [기준컬럼] desc 해서 샘플을 가져온다
+     * @param tableName
+     * @return
+     */
     @Override
     public Page<List> findRecently(Pageable pageable, String tableName) {
 //        Sort.Order order = !pageable.getSort().isEmpty() ? pageable.getSort().toList().get(0) : Sort.Order.by(HUB_LOAD_TM);
@@ -89,6 +111,11 @@ public class QueryRepositoryImpl implements QueryRepository {
         return new PageImpl<List>(list, pageable, count(tableName));
     }
 
+    /**
+     * 기준컬럼이 없는 테이블의 경우 order by 없이 random 샘플을 가져온다
+     * @param tableName
+     * @return
+     */
     @Override
     public List findSample(String tableName) {
         String query = String.format("select * from %s limit %d",tableName, DEFAULT_LIMITS);
@@ -96,6 +123,11 @@ public class QueryRepositoryImpl implements QueryRepository {
         return list;
     }
 
+    /**
+     * 기준컬럼이 없는 테이블의 경우 order by 없이 random 샘플을 가져온다
+     * @param tableName
+     * @return
+     */
     @Override
     public Page<List> findSample(Pageable pageable, String tableName) {
         String query = String.format("select * from %s limit %d offset %d"
