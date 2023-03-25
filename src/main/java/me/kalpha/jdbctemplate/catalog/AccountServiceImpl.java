94870fc0ae9ceb8b07c449fa1cda9f791b68ad79
kalphageek <kalphageek@outlook.com>
2023-03-25 17:50:40 +0900

    Update AccountServiceImpl.java

--- src/main/java/me/kalpha/jdbctemplate/catalog/AccountServiceImpl.java
+++ src/main/java/me/kalpha/jdbctemplate/catalog/AccountServiceImpl.java
@@ -1,7 +1,6 @@
 package me.kalpha.jdbctemplate.catalog;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
